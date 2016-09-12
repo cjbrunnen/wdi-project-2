ea7b21f51d0ad8ea736f1961ada18ed3ba2d710f
cjbrunnen <cazgotsoul@gmail.com>
2016-09-12 09:48:10 +0100

    committing pseudocode stage

---
+++ pseudocode/authenticationPseudo.js
@@ -0,0 +1,150 @@
+app.js:
+
+const express     = require('express');
+const morgan      = require('morgan');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+const mongoose    = require('mongoose');
+
+const app         = express();
+const config      = require('./config/config');
+const router      = require('./config/routes');
+
+mongoose.connect(config.db);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/api', router);
+
+
+app.listen(config.port, () => console.log(`Express started on port: ${config.port}`));
+-------------------------------------------------------------------------------
+model / user.js:
+
+const mongoose = require("mongoose");
+const bcrypt   = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username:     { type: String, unique: true, required: true },
+  email:        { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true }
+});
+
+userSchema
+  .virtual('password')
+  .set(setPassword);
+
+userSchema
+  .virtual("passwordConfirmation")
+  .set(setPasswordConfirmation);
+
+userSchema
+  .path("passwordHash")
+  .validate(validatePasswordHash);
+
+userSchema.methods.validatePassword = validatePassword;
+
+//change the output that the Schema has. you don't need to memorize this part - just keep it handy. this is a BLACKLIST
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    delete ret.passwordHash;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
+
+function setPassword(value){
+  this._password    = value;
+  this.passwordHash = bcrypt.hashSync(value, bcrypt.genSaltSync(8));
+}
+
+function setPasswordConfirmation(passwordConfirmation) {
+  this._passwordConfirmation = passwordConfirmation;
+}
+
+function validatePasswordHash() {
+  if (this.isNew) {
+    if (!this._password) {
+      return this.invalidate("password", "A password is required.");
+    }
+
+    if (this._password !== this._passwordConfirmation) {
+      return this.invalidate("passwordConfirmation", "Passwords do not match.");
+    }
+  }
+}
+
+function validatePassword(password){
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+--------------------------------------------------------------------------------
+controller / authentication.js:
+
+//create two routes. one for register and one for log in
+module.exports = {
+  register: authenticationsRegister,
+  login:    authenticationsLogin
+};
+
+
+const User = require('../models/user');
+//req stands for the http: address. request/response
+function authenticationsRegister(req, res) {
+  User.create(req.body.user, (err, user) => {
+    if (err) return res.status(500).json({ message: 'something went wrong' });
+    return res.status(201).json({
+      message: `Welcome ${user.username}!`,
+      user
+    });
+  });
+}
+
+function authenticationsLogin(req, res) {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).json({ message: 'something went wrong' });
+    if (!user || !user.validatePassword(req.body.password)) {
+      return res.status(401).json({ message: 'Unauthorised' });
+    }
+    return res.status(200).json({
+      message: `Welcome back ${user.username}!`,
+      user
+    });
+  });
+}
+-------------------------------------------------------------------------------
+config / config.js:
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  'db': 'mongodb://localhost/encrypting-passwords-with-bcrypt-and-mongoose'
+};
+-----------------------------------------------------------------------------
+config / routes.js
+
+const express = require('express');
+const router  = express.Router();
+
+const authentications = require('../controllers/authentications.js');
+
+router.route('/register')
+  .post(authentications.register);
+router.route('/login')
+  .post(authentications.login);
+  
+// from drones //
+router.route("/drones")
+  .get(drones.index)
+  .post(drones.create);
+router.route("/drones/:id")
+  .get(drones.show)
+  .put(drones.update)
+  .delete(drones.delete);
+
+
+module.exports = router;
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
---
+++ pseudocode/mainJsPseudo.js
@@ -0,0 +1,169 @@
+// from drones FRONT END //
+
+$(init);
+
+const api_url = "http://localhost:3000/api";
+
+function init(){
+  // Setup the event listeners for the top navigation
+  $(".index").on("click", dronesIndex);
+  $(".new").on("click", dronesNew);
+  // Must use event delegation because the content has been added by JS
+  $("main").on("submit", ".update", dronesUpdate);
+  $("main").on("submit", ".create", dronesCreate);
+  $("main").on("click", ".show", dronesShow);
+  $("main").on("click", ".edit", dronesEdit);
+  $("main").on("click", ".delete", dronesDelete);
+}
+
+function dronesIndex(e) {
+  e.preventDefault();
+  // Grab url
+  let url = $(this).attr("href");
+  // Make the AJAX request to get all of the drones
+  return getDronesForIndex(url);
+}
+
+function getDronesForIndex(url){
+  return $.ajax({
+    method: "GET",
+    url: `${api_url}${url}`,
+  })
+  .done(listDrones);
+}
+
+function listDrones(data){
+  $('#index-container').empty();
+  $.each(data.drones, (i, drone) => {
+    $('#index-container').append(`
+      <li>
+        <ul>
+          <li>
+            <a class="show" href="/drones/${drone._id}">
+              <h3>${drone.name}</h3>
+            </a>
+            <p><img src="${drone.image}"></p>
+            <p>${drone.color}</p>
+            <p>${drone.flightTime}</p>
+          </li>
+        </ul>
+      </li>
+    `);
+  });
+  return showContent("index");
+}
+
+function dronesNew() {
+  // Add the form to the page
+  $("#new-container").html(`
+    <form class="create" method="post" action="/drones">
+    <input type="text" id="name" name="drone[name]" placeholder="Name">
+    <input type="text" id="color" name="drone[color]" placeholder="Color">
+    <input type="text" id="image" name="drone[image]" placeholder="Image">
+    <input type="number" id="flightTime" name="drone[flightTime]" placeholder="Flight Time">
+    <input type="submit" value="Submit">
+  </form>`);
+  return showContent("new");
+}
+
+function dronesCreate() {
+  // Stop the page reloading after the form submission
+  event.preventDefault();
+  // Send the request to create a new drone
+  let method = $(this).attr("method");
+  let action = $(this).attr("action");
+
+  return $.ajax({
+    method: "POST",
+    url: `${api_url}${action}`,
+    data: $(this).serialize()
+  })
+  .done(data => {
+    return getDronesForIndex("/drones");
+  });
+}
+
+function dronesShow(e) {
+  e.preventDefault();
+  let url = $(this).attr("href");
+
+  $.ajax({
+    method: "GET",
+    url: `${api_url}${url}`
+  }).done(data => {
+    let drone = data.drone;
+
+    $('#show-content').html(`
+      <h1>${drone.name}</h1>
+      <p><img src="${drone.image}"></p>
+      <p>${drone.color}</p>
+      <p>${drone.flightTime}</p>
+      <button class="edit" data-id="${drone._id}">Edit</button>
+      <button class="delete" data-id="${drone._id}">Delete</button>
+    `);
+
+    return showContent("show");
+  });
+}
+
+
+function dronesEdit(){
+  event.preventDefault();
+  let id = $(this).data("id");
+  return $.ajax({
+    method: "GET",
+    url: `${api_url}/drones/${id}`
+  }).done(data => {
+    let drone = data.drone;
+    $("#edit-container").html(`
+      <form class="update" method="put" action="/drones/${id}">
+        <input type="text" id="name" name="drone[name]" placeholder="Name" value="${drone.name}">
+        <input type="text" id="color" name="drone[color]" placeholder="Color" value="${drone.color}">
+        <input type="text" id="image" name="drone[image]" placeholder="Image" value="${drone.image}">
+        <input type="number" id="flightTime" name="drone[flightTime]" placeholder="Flight Time" value="${drone.flightTime}">
+        <input type="submit" value="Submit">
+      </form>`);
+
+
+    return showContent("edit");
+  });
+}
+
+function dronesUpdate(e) {
+  e.preventDefault();
+
+  let url    = $(this).attr("action");
+  let method = $(this).attr("method");
+  let data   = $(this).serialize();
+
+  return $.ajax({
+    method: "PUT",
+    url: `${api_url}${url}`,
+    data: data
+  })
+  .done(data => {
+    return getDronesForIndex("/drones");
+  });
+}
+
+function dronesDelete() {
+  event.preventDefault();
+  let id = $(this).data("id");
+
+  return $.ajax({
+    method: "DELETE",
+    url: `${api_url}/drones/${id}`
+  }).done(data => {
+
+    return getDronesForIndex("/drones");
+  });
+}
+
+
+function showContent(id) {
+  // Hide all of the sections
+  $("section").hide();
+  // Show the section that you want to display
+  $(`#${id}-content`).show();
+}
+---------------------------------------------------------------------------
