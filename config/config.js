37b356e584bdbc00f842a4e7a5b9820979a503df
cjbrunnen <cazgotsoul@gmail.com>
2016-09-16 10:57:23 +0100

    added MONGOLAB to config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:   process.env.PORT || 3000,
-  db:     'mongodb://localhost/glutenfree',
+  db:     process.env.MONGOLAB_URI || 'mongodb://localhost/glutenfree',
   secret: process.env.SECRET || 'this information is secret'
 };
