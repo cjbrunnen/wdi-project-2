d293cf95212142a3460207ad074ac6e529ec1197
cjbrunnen <cazgotsoul@gmail.com>
2016-09-16 11:22:34 +0100

    added engines in package.json

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:   process.env.PORT || 3000,
-  db:     process.env.MONGOLAB_URI || 'mongodb://localhost/glutenfree',
+  db:     process.env.MONGODB_URI || 'mongodb://localhost/glutenfree',
   secret: process.env.SECRET || 'this information is secret'
 };
